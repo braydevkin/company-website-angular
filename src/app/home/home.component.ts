0f5f29b9e925e6c97aa08f96c5480f569b0f64c2
Brayan <job.brquirino@gmail.com>
2024-03-02 18:31:41 -0300

    feat: header first structure

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../design/molecules/header/header.component';
+import { HomeTemplate } from '../../templates/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HomeTemplate],
   templateUrl: './home.component.html',
 })
 export class AppComponent {}
