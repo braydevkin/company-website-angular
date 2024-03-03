7fec6c8b19237cbdad1ae8037591b101ea366199
Brayan <job.brquirino@gmail.com>
2024-03-03 15:36:41 -0300

    feat: hero section finished

--- src/design/molecules/hero-action/hero-action.component.ts
+++ src/design/molecules/hero-action/hero-action.component.ts
@@ -3,6 +3,7 @@ import { TitleComponent } from '../../atoms/title/title.component';
 import { SubtitleComponent } from '../../atoms/subtitle/subtitle.component';
 import { RoundedButtonComponent } from '../../atoms/rounded-button/rounded-button.component';
 import { NgOptimizedImage } from '@angular/common';
+import { ReadMoreComponent } from '../../atoms/read-more/read-more.component';
 
 @Component({
   selector: 'app-hero-action',
@@ -11,6 +12,7 @@ import { NgOptimizedImage } from '@angular/common';
     TitleComponent,
     SubtitleComponent,
     RoundedButtonComponent,
+    ReadMoreComponent,
     NgOptimizedImage,
   ],
   templateUrl: './hero-action.component.html',
@@ -19,7 +21,9 @@ export class HeroActionComponent {
   @Input() actionTitle: string =
     'Redirect your traffic with our shortened links';
   @Input() actionSubtitle: string =
-    'The ultimate tool for efficient link management.';
-  @Input() actionButtonTitle: string = 'Register';
+    'An innovative way of dealing with your shortened links, exploit the power of strategies, increase results with AB tests and scale your links quickly and safely.';
+  @Input() callToReadMore: string =
+    "Links in a way you've never seen before, see all our strategies...";
+  @Input() actionButtonTitle: string = 'Start Now !';
   @Input() brandHeroUrl: string = '../../../assets/hero/Illustration.png';
 }
